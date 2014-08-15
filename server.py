3452676ee6624ca27de8417bdbbcd5d4f02173b4
c7h <christoph@gerneth.info>
2014-08-15 15:32:58 +0200

    bootstrap integration

--- server.py
+++ server.py
@@ -6,14 +6,16 @@ Created on 15.08.2014
 from elo_rating import Player
 
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-
-players = [Player("foobar"), Player("Kunis")]
+bootstrap = Bootstrap(app)
 
 
 @app.route("/")
 def mainpage():
+    players = [Player("foobar"), Player("Kunis")]
     return render_template('main.html', players=players )
 
 
